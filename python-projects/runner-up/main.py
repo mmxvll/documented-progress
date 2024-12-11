f2ab2380b7ef0797ffb8c3c59f47e48004c69cdd
a <farhodzodaaa@gmail.com>
2024-12-11 22:39:57 +0500

    update main.py

--- python-projects/runner-up/main.py
+++ python-projects/runner-up/main.py
@@ -1,1 +1,15 @@
+n = 5
+arr = set(map(int, [57,57,-57,-57]))
 
+highest = -1000
+for score in arr:
+    if highest < score:
+        highest = score
+
+
+runner_up = -1000
+for score in arr:
+    if score > runner_up and score != highest:
+        runner_up = score
+
+print(str(runner_up))
