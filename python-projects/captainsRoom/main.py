f6cbefd4a44be16034498f7a91a469161757c1b0
a <farhodzodaaa@gmail.com>
2024-12-12 14:09:53 +0500

    complete captainsRoom

--- python-projects/captainsRoom/main.py
+++ python-projects/captainsRoom/main.py
@@ -1,4 +1,18 @@
 membersPerGroup = int(input())
 roomsList = list(input().split())
 
-print(roomsList)
+duplicates = set()
+seen = set()
+
+for num in roomsList:
+    if num in seen:
+        duplicates.add(num)
+    else:
+        seen.add(num)
+
+captainsRoom = []
+for room in roomsList:
+    if room not in duplicates:
+        captainsRoom.append(room)
+
+print(captainsRoom[0])
