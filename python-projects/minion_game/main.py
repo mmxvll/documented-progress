c202431c1ab0bd9dabdf7624bc40f3b27dbd99dc
a <farhodzodaaa@gmail.com>
2024-12-14 20:29:29 +0500

    create minion_game

---
+++ python-projects/minion_game/main.py
@@ -0,0 +1,8 @@
+def minion_game(s):
+    s = input()
+    # Kevin makes words with vowels
+    # Stuart makes words with consonants
+    # generate algorithm for each player
+    # add points and compare results
+
+    # take string s.
